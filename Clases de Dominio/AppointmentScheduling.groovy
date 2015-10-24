48777410e2db668044714853d4edeb8cb23737c6
Sebastian <smunozca@unal.edu.co>
2015-10-24 16:55:13 -0500

    Clase Agendar Cita

---
+++ Clases de Dominio/AppointmentScheduling.groovy
@@ -0,0 +1,13 @@
+package appet.loca
+
+class AppointmentScheduling
+{
+	String petName
+	String doctor
+	Date date 		//Aca debe contener la hora de la cita
+
+    static constraints =
+    {
+    	
+    }
+}
