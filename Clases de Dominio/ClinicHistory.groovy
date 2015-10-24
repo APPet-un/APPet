c1e822171bafbeef57831f6be559bb634e2bf05e
Sebastian <smunozca@unal.edu.co>
2015-10-24 16:24:59 -0500

    Clase Historia Clinica

---
+++ Clases de Dominio/ClinicHistory.groovy
@@ -0,0 +1,19 @@
+package appet.loca
+
+class ClinicHistory
+{
+	int fileCode
+	Date date
+	String medic
+	String disease
+	String treatment
+	String surgeries
+	String observations
+
+	//Esta clase tiene una relacion algo estraña no se como representarla en groovy
+
+    static constraints =
+    {
+
+    }
+}
