2e83b6fbfa59c55eeb9d3309b4b095ed826d5e6e
Sebastian <smunozca@unal.edu.co>
2015-10-24 16:44:34 -0500

    Clases de Archivos

---
+++ Clases de Dominio/File.groovy
@@ -0,0 +1,15 @@
+package appet.loca
+
+class File
+{
+	int code
+	Date date
+	String name
+
+	//Esta clase se relaciona con hstoria clinica de alguna manera
+
+    static constraints =
+    {
+
+    }
+}
---
+++ Clases de Dominio/LabFile.groovy
@@ -0,0 +1,15 @@
+package appet.loca
+
+class LabFile
+{
+	int code
+	Date date
+	String name
+
+	//Esta clase se relaciona con Laboratorio de alguna manera
+
+    static constraints =
+    {
+
+    }
+}
