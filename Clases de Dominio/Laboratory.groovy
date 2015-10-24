615be2f06e1c5fe2c04d6f9d9dc49e2f805fac87
Sebastian <smunozca@unal.edu.co>
2015-10-24 16:34:30 -0500

    Clase Laboratorio

---
+++ Clases de Dominio/Laboratory.groovy
@@ -0,0 +1,15 @@
+package appet.loca
+
+class Laboratory
+{
+	Date date
+	String diagnostic
+	File attachExams
+
+	//Esta clase tiene una relacion tambien estraña como historia clinica
+
+    static constraints =
+    {
+    	
+    }
+}
