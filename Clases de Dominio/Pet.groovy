a3f4d62951f2e9202e3e523de716a96b1abfbf22
Sebastian <smunozca@unal.edu.co>
2015-10-24 14:58:34 -0500

    Clase Mascota

---
+++ Clases de Dominio/Pet.groovy
@@ -0,0 +1,20 @@
+package appet.loca
+
+class Pet
+{
+	int id
+	String name
+	Date bornDate
+	char gender
+	String species
+	String breed
+	String color
+	String signals
+	boolean microchip
+	File photo
+
+    static constraints =
+    {
+
+    }
+}
