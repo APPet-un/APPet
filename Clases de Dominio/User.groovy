467c92c41d301153fa00a0f63867cf572ef6dcd3
Sebastian <smunozca@unal.edu.co>
2015-10-24 14:20:18 -0500

    Clase Usuario

---
+++ Clases de Dominio/User.groovy
@@ -0,0 +1,18 @@
+package appet.loca
+
+class User
+{
+	int id
+	String firstName
+	String lastName
+	int telephone
+	int movilPhone
+	String address
+	boolean registryType	//True -> Veterinario // False -> Natural
+	boolean type	//True -> Afiliado a Veterinaria // False -> Sin Afiliacion
+
+    static constraints =
+    {
+    	
+    }
+}
