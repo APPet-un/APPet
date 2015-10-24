a67cf7705ae0b48c491933fab3a4a1695cda0a3b
Sebastian <smunozca@unal.edu.co>
2015-10-24 14:24:08 -0500

    Clase Veterinaria

---
+++ Clases de Dominio/Veterinary.groovy
@@ -0,0 +1,17 @@
+package appet.loca
+
+class Veterinary
+{
+	int id
+	String name
+	String address
+	String webPage
+	//Hacer una tabla con el campo de SERVICIOS
+	int telephone
+	int movilPhone
+
+    static constraints =
+    {
+    	
+    }
+}
