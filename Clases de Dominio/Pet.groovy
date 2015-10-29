f9fedeade4844c45874a3ae7966ec839c9c783d1
Storresji <sttorresji@unal.edu.co>
2015-10-29 07:54:21 -0500

    New relations with classes Client and ClinicHistory

--- Clases de Dominio/Pet.groovy
+++ Clases de Dominio/Pet.groovy
@@ -12,17 +12,25 @@ class Pet
 	String signals
 	boolean microchip
 	File photo
+	
+    static hasOne = [clinicHistory: ClinicHistory]
+    static belongsTo = [owner: Client]
 
     static constraints =
     {
     	id (unique:true, blank:false, min:1)
     	name (blank:false, minSize:1)
     	bornDate (blank:false)
+    	gender (blank:false, inList: ["F","M"])
     	gender (blank:false)
     	species (blank:false, minSize:4)
     	breed (blank:false, minSize:5)
     	color (blank:false, minSize:3)
     	signals (blank:true)
     	microchip (blank:false)
+    }
+    static mapping = {
+        id generator: 'increment'
+        //id name: 'pet_id', type: 'integer'
     }
 }
