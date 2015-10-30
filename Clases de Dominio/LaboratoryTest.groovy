f40ad2b145a7c00adefbaa790e3d423f241a9114
Storresji <sttorresji@unal.edu.co>
2015-10-30 07:49:43 -0500

    Update the name to LaboratoryTest and add mapping

--- Clases de Dominio/LaboratoryTest.groovy
+++ Clases de Dominio/LaboratoryTest.groovy
@@ -4,7 +4,7 @@ class Laboratory
 {
 	Date date
 	String diagnostic
-	File attachExams
+	static hasMany = [labFiles: LabFile]
 
 	//Esta clase tiene una relacion tambien estraña como historia clinica
 
@@ -13,4 +13,7 @@ class Laboratory
     	date (blank:false)
     	diagnostic (blank:false, size:5..10000)
     }
+    static mapping = {
+		id generator: 'increment'//, name: 'laboratory_test_id', type: 'integer'
+	}
 }
