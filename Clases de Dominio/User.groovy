4d934a1430b171a56a1fd440c92f09aa8fb78aef
Storresji <sttorresji@unal.edu.co>
2015-10-30 07:48:08 -0500

    Update User.groovy

--- Clases de Dominio/User.groovy
+++ Clases de Dominio/User.groovy
@@ -2,24 +2,25 @@ package appet.loca
 
 class User
 {
-	int id
 	String firstName
 	String lastName
 	int telephone
 	int movilPhone
 	String address
-	boolean registryType	//True -> Veterinario // False -> Natural
-	boolean type	//True -> Afiliado a Veterinaria // False -> Sin Afiliacion
-
+	String emailUser
+	
     static constraints =
     {
-    	id (unique:true, blank:false, min:1)
     	firstName (blank:false, minSize:3)
     	lastName (blank:false, minSize:3)
     	telephone (blank:false, size:8)
     	movilPhone (blank:true, size:10)
     	address (blank:true, minSize:6)
-    	registryType (blank:false)
-    	type (blank:false)
+    	emailUser(blank: false,email: true)
+
     }
+    static mapping = {
+		id generator: 'increment'
+		//id name: 'pet_id', type: 'integer'
+	}
 }
